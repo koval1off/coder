b112cdc77f8d3c9e1f8cd55e7b8aa0dbdaa82e02
Paul <kovaliov0434@gmail.com>
2022-04-14 22:37:37 +0300

    base function 1

--- bank.py
+++ bank.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+
+title = "ATM"
+print(title.center(9,'*'))      #making title
+
+user_id = input("Enter your ID: ")
+user_pass = input("Enter you password: ")
+if int(user_id) == 2222 and int(user_pass) == 3333:     #check if ID and Pass True
+    print("GOOD PASS")
+else:
+    print("Something wrong with your ID or PASS. Try again")
