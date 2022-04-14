56b7b9cf954ea02e97d4a16e11226c08710b54f9
Alex-CourseAndMaterials <54208127+Alex-CourseAndMaterials@users.noreply.github.com>
2022-04-14 16:15:42 -0500

    Update bank.py

--- bank.py
+++ bank.py
@@ -9,6 +9,6 @@ print(title.center(9,'*'))      #making title
 user_id = input("Enter your ID: ")
 user_pass = input("Enter you password: ")
 if int(user_id) == 2222 and int(user_pass) == 3333:     #check if ID and Pass True
-    print("GOOD PASS")
+    print("GOOD PASS. Welcome!")
 else:
     print("Something wrong with your ID or PASS. Try again")
