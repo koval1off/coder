c5aaa1c7a97c0b0f0a0a718e756c106f36c7b12d
aleksandr-krykun <sasha_krikyn97@ukr.net>
2022-04-16 13:36:35 -0500

    minor changes

--- bank.py
+++ bank.py
@@ -4,38 +4,95 @@ time = datetime.now().strftime('%H:%M')
 print(time)
 
 title = "ATM"
-print(title.center(9,'*'))      # making title
+print(title.center(9, '*'))      # making title
 
 user_balance = 5000
-user_id = input("Enter your ID: ")
-user_pass = input("Enter you password: ")
 
-def change_balance():
-    print("You choosed change balance operation!")
+
+def check_id_and_pin(user_id, user_pin):
+    if user_id == 2222 and user_pin == 3333:
+        return True
+
+    return False
+
+
+def change_balance(new_balance):
+    if new_balance < 0:
+        print('Cannot update balance. Reason: New balance is negative')
+        return
+
     global user_balance
-    balance_changes = input("Enter changes you wanna make: ")
-    user_balance += int(balance_changes)   # upgrade balance
-    if user_balance < 0:
-        print("Too much to withdraw!")
-    # else:
-    #     print(f"The new balance is {user_balance}")
-    menu()
+    user_balance = new_balance
+
+
+def upload_money(amount):
+    if amount <= 0:
+        print('You must upload more than 0')
+        return
+
+    new_balance = user_balance + amount
+    change_balance(new_balance)
+
+
+def withdrawal_money(amount):
+    if amount <= 0:
+        print('You must withdrawal more than 0')
+        return
+
+    if user_balance < amount:
+        print('You want to withdrawal more than you have')
+        return
+
+    new_balance = user_balance - amount
+    change_balance(new_balance)
+
 
 def show_balance():
-    print(f"You choosed the Show balance operation!\nYour current balance is {user_balance}")
-    menu()
+    print(f"You choosed the Show balance operation!\n"
+          f"Your current balance is {user_balance}")
+
+
+def get_number_from_input(text):
+    num = input(text)
+
+    return int(num)
+
 
 def menu():
-    print("Menu:\n\t1) Change the balance\n\t2) Show new balance")   # show the menu operations
-    menu_choise = input("Enter the number of operation: ")
-    if int(menu_choise) == 1:
-        change_balance()
-    elif int(menu_choise) == 2:
-        show_balance()
+    while True:
+        print("Menu:\n\t"
+              "1) Upload money\n\t"
+              "2) Withdrawal money\n\t"
+              "3) Show balance\n\t"
+              "4) Exit\n\t")   # show the menu operations
 
-if int(user_id) == 2222 and int(user_pass) == 3333:     # check if ID and Pass True
-    print("GOOD PASS. Welcome!")
-else:
-    print("Something wrong with your ID or PASS. Try again")
+        menu_choise = input("Enter the number of operation: ")
+        if int(menu_choise) == 1:
+            amount = get_number_from_input("Enter amount you wanna upload: ")
+            upload_money(amount)
+            show_balance()
+        elif int(menu_choise) == 2:
+            amount = get_number_from_input("Enter amount you wanna withdrawal: ")
+            withdrawal_money(amount)
+            show_balance()
+        elif int(menu_choise) == 3:
+            show_balance()
+        elif int(menu_choise) == 4:
+            return
+        else:
+            print('Wrong command, try again')
 
-menu()
+
+def main():
+    user_id = get_number_from_input("Enter your ID: ")
+    user_pass = get_number_from_input("Enter you password: ")
+
+    if not check_id_and_pin(user_id, user_pass):
+        print("Something wrong with your ID or PASS. Try again")
+        return
+
+    print('Welcome to our Bank!')
+    menu()
+
+
+main()
