1b040a17b6dcc171d0841871bdaa88a6b9b1bdc9
koval1off <80089497+koval1off@users.noreply.github.com>
2022-04-22 19:19:13 +0300

    fix_3_retries

--- main.py
+++ main.py
@@ -15,16 +15,6 @@ def get_number_from_input(text):
     return int(num)
 
 
-count = 1
-def count_attemps():
-    global count
-    if count < 3:
-        count += 1
-    else:
-        quit()
-    return
-
-
 def menu():
     privat_bank_atm = ATM()
 
@@ -58,21 +48,22 @@ def menu():
 
 
 def main():
-    while True:
+    count_attemps = 1
+    login_success = False
+    while count_attemps <= 3:
         user_id = get_number_from_input("Enter your ID: ")
         user_pass = get_number_from_input("Enter you password: ")
-        try:
-            if not check_id_and_pin(user_id, user_pass):
-                print(f"Something wrong with your ID or PASS. {count}/3. Try again")  # need not to TRY AGAIN at the last time
-                count_attemps()
-                continue
-            else:
-                print('Welcome to our Bank!')
-                menu()
-                break
-        except:
-            print("Try next day!")
+        login_success = check_id_and_pin(user_id, user_pass)
+        if not login_success:
+            print(f"Something wrong with your ID or PASS. {count_attemps}/3. Try again")  # need not to TRY AGAIN at the last time
+            count_attemps += 1
+        else:
             break
+    
+    if login_success:
+        menu()
+    else:
+        print("Try next time. Too much fails")
 
 
 main()
