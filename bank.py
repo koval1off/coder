0ea9682cb9d6450e0114d0512f16f0232f6d7316
koval1off <80089497+koval1off@users.noreply.github.com>
2022-05-06 22:39:44 +0300

    change_balance
    change balance and alfo added discriptions

--- bank.py
+++ bank.py
@@ -1,35 +1,78 @@
+from user import User
+
+
 class ATM:
     def __init__(self):
-        self.user_balance = 5000
+        self.users = []
+        self._read_users()
 
-    def change_balance(self, new_balance):
+    def _read_users(self):
+        """reads file and writes users"""
+        with open("database.txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                info = line.split(":")
+                user = User(info[0], info[1], int(info[2]))
+                self.users.append(user)
+
+    def print_users(self):
+        """prints users' info"""
+        for user in self.users:
+            print(f"User_ID: {user.user_id}; User_PIN: {user.pin}; User_BALANCE: {user.balance}")
+
+    def get_user(self, user_id: str):
+        """checks if the users exists"""
+        for user in self.users:
+            if user.user_id == user_id:
+                return user
+        return None
+
+    def check_pin(self, user: User, user_pin: str) -> bool:
+        """checks if the user's pin is correct"""
+        return user.check_pin(user_pin)
+
+    def get_user_balance(self, user: User) -> int:
+        """give the balance of the current user"""
+        return user.balance
+
+    def change_balance(self, user: User, new_balance):
+        """changes the balance of the current user"""
         if new_balance < 0:
             print('Cannot update balance. Reason: New balance is negative')
             return
 
-        self.user_balance = new_balance
+        user.balance = new_balance
+        self.save_users()
 
-    def upload_money(self, amount):
+    def upload_money(self, user: User, amount: int):
+        """adds the amount to the balance of current user"""
         if amount <= 0:
             print('You must upload more than 0')
             return
 
-        new_balance = self.user_balance + amount
+        new_balance = user.balance + amount
+        self.change_balance(user, new_balance)
 
-        self.change_balance(new_balance)
-
-    def withdrawal_money(self, amount):
+    def withdrawal_money(self, user: User, amount: int):
+        """takes the amount from the balance of current user"""
         if amount <= 0:
             print('You must withdrawal more than 0')
             return
-
-        if self.user_balance < amount:
+        if user.balance < amount:
             print('You want to withdrawal more than you have')
             return
 
-        new_balance = self.user_balance - amount
-        self.change_balance(new_balance)
+        new_balance = user.balance - amount
+        self.change_balance(user, new_balance)
 
-    def show_balance(self):
+    def show_balance(self, user: User):
+        """shows the user's current balance"""
         print(f"You choosed the Show balance operation!\n"
-              f"Your current balance is {self.user_balance}")
+              f"Your current balance is {user.balance}")
+
+    def save_users(self):
+        """loads all information about users"""
+        with open("database.txt", "w") as f:
+            for user in self.users:
+                f.write(f"{user.user_id}:{user.pin}:{user.balance}\n")
+
--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from bank import ATM
 from datetime import datetime
+from user import User
 
 
-with open("database.txt", "w") as f:    # just to create the file automatically 
-    f.write("2222:3333\n")
-    f.write("5555:6666\n")
-
-
-def check_id_and_pin(user_id, user_pin):
-    with open("database.txt", "r") as base:
-        for line in base.readlines():
-            line = line.split(":")
-            if str(user_id) in line[0]:
-                if str(user_pin) in line[1]:
-                    return True
-
-
-def get_number_from_input(text):
+def get_number_from_input(text: str) -> int:
     try:
         num = input(text)
         return int(num)
@@ -24,7 +11,7 @@ def get_number_from_input(text):
         print("Use 4 digits!")
 
 
-def validation_credential(user_id, user_pin):
+def validation_credential(user_id: str, user_pin: str) -> bool:
     if len(str(user_id)) == 4 and len(str(user_pin)) == 4:
         return True
     else:
@@ -32,8 +19,12 @@ def validation_credential(user_id, user_pin):
         return False
 
 
-def menu():
-    privat_bank_atm = ATM()
+def menu(user: User, atm: ATM):
+    if user is None:
+        return
+
+    if atm is None:
+        return
 
     while True:
         time = datetime.now().strftime('%H:%M')
@@ -50,14 +41,14 @@ def menu():
         menu_choise = input("Enter the number of operation: ")
         if int(menu_choise) == 1:
             amount = get_number_from_input("Enter amount you wanna upload: ")
-            privat_bank_atm.upload_money(amount)
-            privat_bank_atm.show_balance()
+            atm.upload_money(user, amount)
+            atm.show_balance(user)
         elif int(menu_choise) == 2:
             amount = get_number_from_input("Enter amount you wanna withdrawal: ")
-            privat_bank_atm.withdrawal_money(amount)
-            privat_bank_atm.show_balance()
+            atm.withdrawal_money(user, amount)
+            atm.show_balance(user)
         elif int(menu_choise) == 3:
-            privat_bank_atm.show_balance()
+            atm.show_balance(user)
         elif int(menu_choise) == 4:
             return
         else:
@@ -65,15 +56,19 @@ def menu():
 
 
 def main():
+    atm = ATM()
+    user = None
     count_attemps = 1
     login_success = False
 
     while count_attemps <= 3:
-        user_id = get_number_from_input("Enter your ID: ")
-        user_pass = get_number_from_input("Enter your PASS: ")
+        user_id = input("Enter your ID: ")
+        user_pass = input("Enter your PASS: ")
 
         if validation_credential(user_id, user_pass):
-            login_success = check_id_and_pin(user_id, user_pass)
+            user = atm.get_user(user_id)
+            if user:
+                login_success = atm.check_pin(user, user_pass)
             if not login_success:
                 error_text = f"Wrong ID or PASS. {count_attemps}/3."
                 print(error_text + ' Try again') if count_attemps < 3 else print(error_text)
@@ -83,8 +78,8 @@ def main():
         else:
             continue
     
-    if login_success:
-        menu()
+    if login_success and user:
+        menu(user, atm)
     else:
         print("Try next time. Too much fails")
 
